d481bef4bbed1b35b489cf9650447d1faaf93a51
Fares Ben Amor <48929657+faresbenamor@users.noreply.github.com>
2023-07-26 11:29:12 +0200

    normalize

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,13 +14,14 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
-streamlit.text(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalized)
