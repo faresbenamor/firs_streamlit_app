d8e5800e846c0165d4a1db43eb098542047d84e2
Fares Ben Amor <48929657+faresbenamor@users.noreply.github.com>
2023-07-26 11:19:46 +0200

    Update streamlit_app.py
    add requests

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import requests
 import pandas
 
 streamlit.title('My Parents New Healty Dinner')
@@ -8,6 +9,7 @@ streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+# Pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -15,5 +17,10 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
+# Requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+streamlit.text(fruityvice_response)
+
