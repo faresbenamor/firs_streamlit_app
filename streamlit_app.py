e6e2be699c3059b9f9fcf4572a014155e08b05b3
Fares Ben Amor <48929657+faresbenamor@users.noreply.github.com>
2023-07-27 17:52:44 +0200

    ffff

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ except URLError as e:
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
          my_cur.execute("use role accountadmin")
-         my_cur.execute("insert into fruit_load_list values(%s)", new_fruit)
+         my_cur.execute("insert into fruit_load_list values('"+ new_fruit +"')")
          return 'Thanks for adding ' + new_fruit
 
 # Text entry box 2
