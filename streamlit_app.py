c8073323b925de51843adec32998479c48c9c1bf
Fares Ben Amor <48929657+faresbenamor@users.noreply.github.com>
2023-07-27 16:34:33 +0200

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import requests
 import pandas
 import snowflake.connector
-from urllib import URLError
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healty Dinner')
 streamlit.header('🥣 Breakfast Menu')
