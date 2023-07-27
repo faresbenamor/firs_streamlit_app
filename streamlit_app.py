8156cfa51b87ae2ad3d8090587a8f19f137c3a2a
Fares Ben Amor <48929657+faresbenamor@users.noreply.github.com>
2023-07-27 17:31:22 +0200

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,13 +44,17 @@ try:
 except URLError as e:
     streamlit.error()
 
-streamlit.stop()
-
-# Snowflake
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("use role accountadmin")
-my_cur.execute("insert into fruit_load_list values('test')")
-my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains :")
-streamlit.dataframe(my_data_rows)
+# Snowflake call function
+def get_fruit_load_list():
+    with my_cnx.cursor() as my_cur:
+    my_cur.execute("use role accountadmin")
+    my_cur.execute("select * from fruit_load_list")
+    return my_cur.fetchall()
+
+# Create button to load fruits
+if streamlit.button('Get Fruit Load List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()
+    streamlit.stop()
+    streamlit.dataframe(my_data_rows)
