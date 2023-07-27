0faa9ea4b0ca4455bb9265906532d6f8c9bbf3b2
Fares Ben Amor <48929657+faresbenamor@users.noreply.github.com>
2023-07-27 11:24:57 +0200

    fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import requests
 import pandas
-import snowflake-connector-python
+import snowflake.connector
 
 streamlit.title('My Parents New Healty Dinner')
 streamlit.header('🥣 Breakfast Menu')
