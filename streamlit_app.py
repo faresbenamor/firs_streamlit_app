54fa24db5cc801728417764db5e192e897c4db56
Fares Ben Amor <48929657+faresbenamor@users.noreply.github.com>
2023-07-27 17:47:35 +0200

    jj

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,8 +48,8 @@ except URLError as e:
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
          my_cur.execute("use role accountadmin")
-         my_cur.execute("insert into fruit_load_list values('from streamlit')")
-         return 'Thanks for adding' + new_fruit
+         my_cur.execute("insert into fruit_load_list values('"+ new_fruit +"')")
+         return 'Thanks for adding ' + new_fruit
 
 # Text entry box 2
 new_fruit_to_add = streamlit.text_input('What fruit would you like to add ?')
